7ae6dd10f17fdcadbe8e9dd096d7aec5b9b630f9
manueo <mail@manueo.com>
2025-04-10 22:14:49 +0200

    updated timestamps in texts migration to use DATETIME type

--- src/database/migrations/text.php
+++ src/database/migrations/text.php
@@ -4,8 +4,8 @@ return [
     'up' => '
             CREATE TABLE IF NOT EXISTS texts (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 `key` VARCHAR(255) NOT NULL,
                 `text` TEXT NOT NULL,
                 `language` VARCHAR(2) NOT NULL,       
