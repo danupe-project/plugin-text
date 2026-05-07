d796e9e29cdff0391cfe32314a737613a7ac5191
manueo <mail@manueo.com>
2026-05-07 18:17:54 +0200

    removed dummy seed

--- src/database/seeds/text.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'up' => '
-        INSERT INTO texts (`key`,`text`,`language`) VALUES ("test","test","de");
-    ',
-    'down' => '
-        DELETE FROM texts WHERE id = 1;
-    '
-];
